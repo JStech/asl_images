14b70ff2fa647b0dea3153d3dd45f054866b2267
John Stechschulte <john.l.stechschulte@gmail.com>
2016-11-26 17:13:43 -0500

    u23 montaged

--- process.sh
+++ process.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-rename 's/ /_/g' $1/*.jpg
+#rename 's/ /_/g' $1/*.jpg
 
-for f in $1/*.jpg; do
-  convert "$f" -crop 720x720+280+0 $2/$(basename "$f")
-done
+#for f in $1/*.jpg; do
+  #convert "$f" -crop 720x720+280+0 $2/$(basename "$f")
+#done
 
-for f in $2/*_1.jpg; do
-  w=$(echo $f | sed 's/_1.jpg$//')
-  montage -geometry +0+0 ${w}_[1-9].jpg ${w}.jpg
-  rm ${w}_[1-9].jpg
+for f in *_01.jpg; do
+  w=$(echo $f | sed 's/_01.jpg$//')
+  montage -geometry +0+0 ${w}_0[1-9].jpg ${w}.jpg
+  rm ${w}_0[1-9].jpg
 done
